753716ff03ca7395ac825c0c51725e0b91641989
liamH47 <liamhanafee@gmail.com>
2021-03-05 21:39:19 -0500

    job model, get +post routes and controller actions done

--- server/controllers/jobs.js
+++ server/controllers/jobs.js
@@ -1,3 +1,22 @@
-export const getJobs = (req, res) => {
-    res.send('this works')
+import Job from '../models/job.js'
+
+export const getJobs = async (req, res) => {
+    try {
+        const jobs = await Job.find();
+        console.log(jobs);
+        res.status(200).json(jobs);
+    } catch (error) {
+        res.status(404).json({ message: error.message})
+    }
+}
+
+export const createJob = async (req, res) => {
+    const job = req.body;
+    const newJob = new Job(job)
+    try {
+        await newJob.save();
+        res.status(201).json(newJob);
+    } catch (error) {
+        res.status(409).json({ message: error.message})
+    }
 }
--- server/routes/jobs.js
+++ server/routes/jobs.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { getJobs } from '../controllers/jobs.js'
+import { getJobs, createJob } from '../controllers/jobs.js'
 
 const router = express.Router();
 
 router.get('/', getJobs);
+router.post('/', createJob);
 
 export default router;
