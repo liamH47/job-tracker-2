837236f0dbfa49804b0fe0b4e0a110cf19256ffd
liamH47 <liamhanafee@gmail.com>
2021-03-07 17:29:10 -0500

    fixed cors order in index.js

--- server/index.js
+++ server/index.js
@@ -9,11 +9,11 @@ import jobRoutes from './routes/jobs.js'
 const app = express();
 dotenv.config()
 
-app.use('/jobs', jobRoutes);
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+app.use('/jobs', jobRoutes);
 
 const PORT = process.env.PORT || 5000;
 
